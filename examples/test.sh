d85600e38ea41fb127015a25a083887bd7ca2509
fgvieira <fgarrettvieira@gmail.com>
2014-06-30 17:46:05 +0200

    Updated tests for new ANGSD version

--- examples/test.sh
+++ examples/test.sh
@@ -1,11 +1,33 @@
+
+N_SITES=10000
+ERR=0.01
+DEPTH=4
+P_VAR=1
+SEED=12345
+
+# Clean-up
+rm -f testA* testF.*
+
+
+
 # Simulate 3 populations with no inbreeding
-../ngsSim -npop 3 -nind 10 8 6 -nsites 10000 -errate 0.01 -depth 4 -pvar 1 -mfreq 0.005 -F 0.3 0.1 -model 1 -seed 12345 -outfiles testA
+../ngsSim -npop 3 -nind 10 8 6 -nsites $N_SITES -errate $ERR -depth $DEPTH -pvar $P_VAR -mfreq 0.005 -F 0.3 0.1 -model 1 -seed $SEED -outfiles testA
+
+
 
 # Setting up inbreeding file
 perl -s -e 'foreach(1..$n_ind){$x=0; $x+=0.25 if($_/$n_ind > 0.2); $x+=0.25 if($_/$n_ind > 0.4); $x+=0.25 if($_/$n_ind > 0.6); $x+=0.25 if($_/$n_ind > 0.8); $x+=0.25 if($_/$n_ind > 1); print $x."\n"}' -- -F=$F -n_ind=20 > testF.indF
 # Simulate 1 population with inbreeding
-../ngsSim -npop 1 -nind 20 -nsites 10000 -errate 0.01 -depth 4 -pvar 1 -mfreq 0.005 -F testF.indF -model 1 -seed 12345 -outfiles testF
+../ngsSim -npop 1 -nind 20 -nsites $N_SITES -errate $ERR -depth $DEPTH -pvar $P_VAR -mfreq 0.005 -F testF.indF -model 1 -seed $SEED -outfiles testF
 
+
+
+# Create ANC fasta and fai files for unfolded SAF analyses
+perl -s -e 'print(">chrSIM\n".("A"x$n)."\n");' -- -n=$N_SITES > testAF.ANC.fas
+echo -e "chrSIM\t$N_SITES\t8\t50\t51" > testAF.ANC.fai
+
+
+##### CHECK
 # Test MD5 checksum
 md5sum testA* testF.* | sort -k 2,2 > /tmp/test.md5
 
